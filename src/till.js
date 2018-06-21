4c22c823de163f93991badb488a14c4537cd5dd9
Joel Goodwin <joeldaniel@hotmail.co.uk>
2018-06-21 20:21:38 +0100

    Refactored protoyypes for scan and addtobasket

--- src/till.js
+++ src/till.js
@@ -1,28 +1,47 @@
-const scan = (barcode, items) => {
-  return items.find((item) => {
-    return item.barcode === barcode;
-  });
+function Till () {
+
+  this.items = [
+
+  { fruit: 'apple',
+    barcode: 123,
+    price: 5
+  },
+
+  { fruit: 'banana',
+    barcode: 456,
+    price: 6
+  },
+  
+  { fruit: 'orange',
+    barcode: 789,
+    price: 7,
+  },
+  
+  { fruit: 'pineapple',
+    barcode: 5367,
+    price: 80,
+  },
+  
+  { fruit: 'kiwi',
+    barcode: 765,
+    price: 25,
+  }
+
+  ];
+
+  this.basket = [];
+
 }
 
-const addToBasket = (item, basket) => {
-  basket.push(item);
-}
-
-const totalPrice = (basket) => {
-  return basket.reduce((total, item) => {
-    return total + item.price;
-  }, 0);
-}
-
-const removeFromBasket = (barcode, basket) => {
-  const item = scan(barcode, basket);
-  const index = basket.indexOf(item);
-  basket.splice(index, 1);
-}
-
-module.exports = {
-  scan,
-  addToBasket,
-  totalPrice,
-  removeFromBasket,
+Till.prototype.scan = function (barcode) {
+  return this.items.find((item => item.barcode === barcode)
+  ).fruit;
 };
+
+Till.prototype.addToBasket = function (item) {
+  return this.basket.push(item);
+}
+
+
+
+module.exports = Till;
